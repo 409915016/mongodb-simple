6f0b894f834f835e317a4caadb74af40af64f64c
Mather <409915016@qq.com>
2018-02-11 18:06:21 +0800

    * Nesting Sub Documents.

--- test/connection.js
+++ test/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // ES6 Promises
-//mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 // Connect to the db before tests run
 before(function (done) {
--- test/deleting_test.js
+++ test/deleting_test.js
@@ -1,33 +1,27 @@
 const assert = require('assert');
 const MarioChar = require('../models/mariochar');
 
-// Describe tests
-describe('Deleting records', function () {
-
+// Describe our tests
+describe('Deleting records', function(){
   var char;
-
-  beforeEach(function (done) {
+  // Add a character to the db before each tests
+  beforeEach(function(done){
     char = new MarioChar({
-      name: 'Mario',
+      name: 'Mario'
     });
-
-    char.save().then(function () {
+    char.save().then(function(){
       done();
     });
   });
 
   // Create tests
-  it('Deletes one record from the database', function (done) {
-    MarioChar.findOneAndRemove({
-      name: 'Mario'
-    }).then(function () {
-      MarioChar.findOne({
-        name: 'Mario'
-      }).then(function (res) {
-        assert(res === null);
+  it('Deletes a record from the database', function(done){
+    MarioChar.findOneAndRemove({name: 'Mario'}).then(function(){
+      MarioChar.findOne({name: 'Mario'}).then(function(result){
+        assert(result === null);
         done();
-      })
-    })
+      });
+    });
   });
 
 });
