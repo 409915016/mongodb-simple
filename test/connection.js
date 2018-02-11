50f5ef39ef74af0120773551ffff48c106c83f5c
Mather <409915016@qq.com>
2018-02-11 17:01:19 +0800

    * Deleting Records.

--- test/connection.js
+++ test/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// ES6 Promises
+//mongoose.Promise = global.Promise;
+
 // Connect to the db before tests run
 before(function (done) {
   // Connect to mongodb
